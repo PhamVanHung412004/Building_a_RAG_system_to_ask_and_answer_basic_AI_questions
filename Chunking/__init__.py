dc79215745e494b568d2053ff8a5b39c0b626a3b
PhamVanHung412004 <hp36780@gmail.com>
2025-03-22 15:33:05 +0700

    fix folder module and remove foler main

--- Chunking/__init__.py
+++ Chunking/__init__.py
@@ -1,1 +1,39 @@
-import Chunking
+from llama_index.core.node_parser import (
+    SentenceSplitter,
+    SemanticSplitterNodeParser,
+)
+
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.core import SimpleDirectoryReader
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+import os
+import torch
+
+
+class Chunking_Data:
+    def __init__(self,name_model : str,documents : list) -> None:
+        '''
+        name_model : Tên Model Embedding sẽ chọn để chunking
+        documents : Văn bản sau khi đã chuyển hóa từ file PDF thành file text và được lưu dưới dạng list
+        '''
+        self.__name_model: str = name_model
+        self.__documents : list = documents
+
+    def Get_nodes(self) -> list:        
+        try:
+            device_type = "cuda" if torch.cuda.is_available() else "cpu"
+
+            embed_model = HuggingFaceEmbedding(model_name=self.__name_model, device=device_type)
+
+            splitter = SemanticSplitterNodeParser(
+                buffer_size=1,
+                breakpoint_percentile_threshold=95,
+                embed_model=embed_model
+            )
+            nodes = splitter.get_nodes_from_documents(self.__documents)
+            return nodes
+        except ZeroDivisionError as e:
+            print("Error: {}".format(e))
+
+
--- Get_datas/__init__.py
+++ Get_datas/__init__.py
@@ -1,1 +1,14 @@
-import Get_datas
+from llama_index.core import SimpleDirectoryReader
+class Get_Data:
+    def __init__(self, path_data: str) -> None:
+        self.path_data: str = path_data
+        '''
+        path_data : Đường dẫn đến folder chứa file pdf cần sử dụng
+        '''
+    def run(self) -> list:
+        
+        try:
+            return SimpleDirectoryReader(self.path_data).load_data()    
+        except ZeroDivisionError as e:
+            print(e)
+
