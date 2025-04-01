37de47acc30124ed1d3260de333e44bfdc7e9501
PhamVanHung412004 <hp36780@gmail.com>
2025-04-01 10:44:25 +0700

    no name

--- deploy/RAG.py
+++ deploy/RAG.py
@@ -6,6 +6,9 @@ from package import Path
 from package import BM25Okapi
 from package import SentenceTransformer
 from package import np
+import time
+
+
 def load_model():
     model = SentenceTransformer("all-MiniLM-L6-v2")  
     return model
@@ -18,14 +21,21 @@ def main():
     bm25 = BM25Okapi(tokenized_docs)
 
     use_query = input("Enter Quesition: ")
+
+    start = time.perf_counter()
+# Đoạn code cần đo
     
     result_keyword_search = keyword_search(use_query,3,bm25).run()
-    vector1 = [int(x) for x in result_keyword_search]
+    # vector1 = [int(x) for x in result_keyword_search]
     
     result_semantic_search = Sematic_search(load_model(),use_query,3).run()
-    vector2 = [int(x) for x in result_semantic_search]
+    # vector2 = [int(x) for x in result_semantic_search]
     
     Hybird_search = init_Hybrid_Search(use_query,3,documents,result_keyword_search,result_semantic_search).run()
-    print(Hybird_search[0])
+    print(Hybird_search)
+    end = time.perf_counter()
+
+    print(f"Thời gian chạy: {end - start:.6f} giây")
+
     # print(result_semantic_search)
 main()
